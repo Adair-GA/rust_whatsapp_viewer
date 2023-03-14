039b507b91c4ec438b2a8c59411b41759fcd8a25
Adair Gondan <adairyves@gmail.com>
2023-03-14 13:28:35 +0100

    quoted message is now an Rc

--- src/database/message.rs
+++ src/database/message.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use rusqlite::{Row, Error};
 
 #[derive(Debug)]
@@ -10,19 +12,19 @@ struct Location {
 struct Media {}
 
 #[derive(Debug)]
-pub struct Message<'a>{
+pub struct Message{
     id: String,
     from_me: bool,
     text_data: Option<String>,
     timestamp: i64,
-    quoted: Option<&'a Message<'a>>,
+    quoted: Option<Rc<Message>>,
     location: Option<Location>,
     media: Option<Media>
 }
 
 
-impl<'a> Message<'a> {
-    pub fn from_row(r: &Row, already_existing: &'a Vec<Message>) -> Result<Message<'a>,Error> {
+impl Message {
+    pub fn from_row(r: &Row, already_existing: &Vec<Rc<Message>>) -> Result<Message,Error> {
         let id = r.get::<usize,String>(0)?;
         let from_me = r.get::<usize,i32>(1)? == 1;
         let text_data = r.get::<usize,Option<String>>(2)?;
@@ -45,7 +47,7 @@ impl<'a> Message<'a> {
 
         let quoted = if let Some(quoted) = quoted {
             if let Some(quoted) = Message::get_quoted_message(quoted, already_existing) {
-                Some(quoted)
+                Some(Rc::clone(quoted))
             }
             else{
                 None
@@ -69,11 +71,11 @@ impl<'a> Message<'a> {
     }
     
 
-    fn get_quoted_message(quoted_id: String, already_existing: &'a Vec<Message>) -> Option<&'a Message<'a>> {
+    fn get_quoted_message(quoted_id: String, already_existing: &Vec<Rc<Message>>) -> Option<&Rc<Message>> {
         let mut it = already_existing.iter().rev();
         while let Some(ms) = it.next_back() {
             if ms.id == quoted_id {
-                return Some(&ms)
+                return Some(ms)
             }
         }
         None
