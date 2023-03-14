4e16c93f9db86492af20f9f9058eda73267058ae
Adair Gondan <adairyves@gmail.com>
2023-03-14 13:29:17 +0100

    gets a chats message and prints them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{ io, };
+
 pub mod database;
 
 fn main() {
@@ -5,8 +7,30 @@ fn main() {
     if let Err(res) = db.validate().map_err(|e| e.to_string()){
         panic!("{}", res)
     }
-    let chats = db.get_chats().unwrap();
-    for c in chats {
-        println!("{:?}", c)
+    let mut chats = db.get_chats().unwrap();
+    
+
+
+    // for c in chats {
+    //     println!("{:?}", c)
+    // }
+    
+    //get a number from keyboard
+    let mut input = String::new();
+    println!("Enter a number: ");
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim().parse::<i32>().unwrap();
+
+    //get the chat with the given id
+    let chat = chats.get_mut(&input).unwrap();
+    println!("{:?}", chat);
+
+    //get the messages of the chat
+    db.get_messages_of_chat(chat).unwrap();
+    
+    for i in 1..100 {
+        let message = chat.get_message_by_index(i).unwrap().clone();
+        println!("{:?}", message)
     }
+
 }
